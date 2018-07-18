c6fb8e817b0f52af7050b21d76acc367824d6f96
root <root@vagrant-ubuntu-trusty.vagrantup.com>
2018-07-18 10:15:33 +0000

    Kapitel Integration TYPO3

---
+++ typo3conf/ext/typo3training/Configuration/TypoScript/Library/0010_config.ts
@@ -0,0 +1,8 @@
+config {
+	language = de
+	locale_all = de_DE.utf8
+	sys_language_uid = 0
+
+	# Eindeutigere Fehlermeldungen im Frontend
+	contendObjectExceptionHandler = 0
+}
---
+++ typo3temp/var/Cache/Code/fluid_template/partial_Jumbotron_4261baf85b6155913e99165d933358c12ef67591.php
@@ -0,0 +1,48 @@
+<?php
+
+class partial_Jumbotron_4261baf85b6155913e99165d933358c12ef67591 extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate {
+
+public function getLayoutName(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this; 
+return (string) '';
+}
+public function hasLayout() {
+return FALSE;
+}
+public function addCompiledNamespaces(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$renderingContext->getViewHelperResolver()->addNamespaces(array (
+  'core' => 
+  array (
+    0 => 'TYPO3\\CMS\\Core\\ViewHelpers',
+  ),
+  'f' => 
+  array (
+    0 => 'TYPO3Fluid\\Fluid\\ViewHelpers',
+    1 => 'TYPO3\\CMS\\Fluid\\ViewHelpers',
+  ),
+  'formvh' => 
+  array (
+    0 => 'TYPO3\\CMS\\Form\\ViewHelpers',
+  ),
+));
+}
+
+/**
+ * Main Render function
+ */
+public function render(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+
+return '<!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+      </div>
+    </div>';
+}
+
+
+}
+#
